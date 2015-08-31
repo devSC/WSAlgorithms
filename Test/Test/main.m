c742180536d58b9d9fd2acac21665de6d7c49858
袁仕崇 <xiaochong2154@163.com>
2015-08-31 23:38:10 +0800

    ^_^

---
+++ Test/Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Wilson-Yuan on 15/8/31.
+//  Copyright (c) 2015年 wilson-yuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- WSAlgorithms/WSAlgorithms/main.m
+++ WSAlgorithms/WSAlgorithms/main.m
@@ -7,11 +7,76 @@
 //
 #include <stdio.h>
 
+int a[101], n; //
 //快速排序
 void quicksorts(int left, int right) {
-    printf("%d %d", left, right);
+//    printf("%d %d", left, right);
+    
+    int i, j, t, temp;
+    
+    if (left > right) {
+        return;
+    }
+    
+    temp = a[left]; //temp中存的就是基数
+    i = left;
+    j = right;
+    while (i != j) {
+        
+        //顺序很重要, 先从右向左
+        while (a[j] >= temp && i < j) {
+            j--;
+        }
+        
+        while (a[i]<= temp && i < j) {
+            i++;
+        }
+        
+        //交换两个数在数组中的位置
+        
+        if (i < j) { //当哨兵i和哨兵j没有相遇时,
+            
+            t = a[i];
+            a[i] = a[j];
+            
+            a[j] = t;
+            
+        }
+        printf("a[%d]: %d a[%d]: %d", i, a[i], j, a[j]);
+    }
+    
+    //最终,将基数归位
+    a[left] = a[i];
+    a[i] = temp;
+    
+    quicksorts(left, i - 1); //继续处理左边 这是递归过程
+    quicksorts(i + 1, right);//继续处理右边 这是递归过程
+    
+    return;
 }
 
 int main(int argc, const char * argv[]) {
-    quicksorts(2, 5);
+    
+    @autoreleasepool {
+        
+        printf("Hello world");
+        
+        int i;
+        //
+        scanf("%d",&n);
+        
+        for ( i = 0; i < n; i ++) {
+            scanf("%d", &a[i]);
+        }
+        
+        quicksorts(1, n);
+        
+        for (int i = 0; i < n; i ++) {
+            printf("%d", a[i]);
+        }
+
+    }
+    
+    
+    return 0;
 }

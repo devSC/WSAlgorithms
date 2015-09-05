6e436517dbea2750ad40523003d4d2f284958fb1
袁仕崇 <xiaochong2154@163.com>
2015-09-05 22:19:54 +0800

    Update

--- WSAlgorithms/WSAlgorithms/main.m
+++ WSAlgorithms/WSAlgorithms/main.m
@@ -124,6 +124,224 @@ void ws_queue() {
     
     getchar();
 }
+
+void ws_stack() { //或许答案是错误的 p.32
+    
+    char a[101], s[101];
+    
+    int i, len, mid, next, top;
+    
+    gets(a); //读入一行字符串
+    //gets(<#char *#>)
+    scanf("%c", a);
+    
+    len = (int)strlen(a);
+    
+    mid = len/2 - 1;
+    
+    
+    top = 0;//栈的初始化
+    
+    //将mid前的字符串依次入栈
+    for (i = 0; i <= mid; i ++) {
+        s[++top] = a[i];
+    }
+    
+    //判断字符串数组的奇偶数, 并找出需要进行字符串匹配的起始下标
+    
+    if (len % 2 ==0) {
+        next = mid + 1;
+    } else {
+        next = mid + 2;
+    }
+    
+    
+    //开始匹配
+    
+    for (i = next; i < len - 1; i ++) {
+        if (a[i] != s[top]) {
+            break;
+        }
+        top--;
+    }
+    
+    //如果top为0 则说明每个数都检查过去了
+    if (top == 0) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+}
+
+void ws_xiaomoyouxi() {
+    struct queue
+    {
+        
+        int data[10000]; //储存队列中的元素
+        int head; //储存对头
+        int tail; //储存队尾
+    };
+    
+    
+    struct stack
+    {
+        int data[10];
+        int top;
+    };
+    
+    
+    struct queue q1,q2; //q1为小哼 q2小哈
+    struct stack s;
+    int i, t;
+    int book[10];
+    
+    
+    
+    q1.head = 1;
+    q1.tail = 1;
+    q2.head = 1;
+    q2.tail = 1;
+    
+    
+    //初始化栈
+    s.top = 0;
+    
+    for (i = 0; i <= 9; i++) {
+        book[i] = 0;
+    }
+    
+    printf("请输入小哼手中的牌(6张 1-9的数): ");
+    for (i = 1; i <= 6; i ++) {
+        scanf("%d", &q1.data[q1.tail]);
+        q1.tail ++;
+        
+    }
+    
+    
+    printf("请输入小哈手中的牌(6张 1-9的数): ");
+    for (i = 1; i <=6; i++) {
+        scanf("%d", &q2.data[q2.tail]);
+        q2.tail ++;
+    }
+    
+    
+    while (q1.head < q1.tail && q2.head < q2.tail) { //当队列不为空的时候, 执行死循环
+        
+        t = q1.data[q1.head]; //Q1出牌
+        if (book[t] == 0) { //表明没有相同的pai
+            
+            //小哼没有赢牌
+            q1.head++; //已打出一张, So,head向后罗一位
+            s.top++;
+            s.data[s.top] = t; //把打出的牌放到桌子上 入栈
+            book[t] = 1;
+        } else {
+            
+            //赢牌
+            q1.head++;
+            q1.data[q1.tail] = t; //将打出的牌放进最后
+            q1.tail++;
+            
+            while (s.data[s.top] != t)//将可以赢得的牌放到手中排的末尾
+            {
+                
+                book[s.data[s.top]] = 0; //取消标记
+                q1.data[q1.tail] = s.data[s.top];
+                
+                q1.tail++;
+                s.top--;
+                
+            }
+            
+            //收回桌上牌面为t的牌
+            book[s.data[s.top]] = 0;
+            q1.data[q1.tail] = s.data[s.top];
+            q1.tail ++;
+            s.top --;
+            
+        }
+        
+        if (q1.head == q1.tail) {
+            break; //小哼手上的牌已经出完 游戏结束;
+        }
+        
+        
+        t = q2.data[q2.head];//小哈出牌
+        
+        if (book[t] == 0) { //没有相同的牌, 所以入栈
+            
+            q2.head++;
+            s.top++;
+            s.data[s.top] = t;
+            book[t] = 1;
+            
+        } else if (book[t] == 1){
+            
+            //小哈可以赢牌
+            q2.head++;
+            q2.data[q2.tail] = t;
+            
+            q2.tail ++;
+            
+            while (s.data[s.top] != t) {
+                book[s.data[s.top]] = 0;
+                q2.data[q2.tail] = s.data[s.top]; //放入队尾
+                q2.tail++;
+                s.top --;
+                
+            }
+            
+            //
+            book[s.data[s.top]] = 0;
+            q2.data[q2.tail] = s.data[s.top];
+            q2.tail++;
+            s.top--;
+            
+        }
+        
+    }
+    
+    
+    if (q2.head == q2.tail) {
+        
+        printf("小哼Win \n");
+        
+        printf("小哼当前手中的牌是: ");
+        for (i = q1.head; i <= q1.tail -1; i ++) {
+            printf("%d", q1.data[i]);
+        }
+        
+        if (s.top > 0) { //如果桌上有牌则依次输出牌
+            
+            printf("桌上的牌是: ");
+            for (i = 0; i <= s.top; i ++) {
+                printf(" %d", s.data[i]);
+            }
+        } else {
+            printf("桌子上没有牌");
+        }
+        
+    } else {
+        printf("小哈Win");
+        printf("小哈当前的牌是: ");
+        
+        for (i = q2.head; i <= q2.tail -1; i ++) {
+            printf(" %d", q2.data[i]);
+        }
+        
+        if (s.top > 0) { //如果桌上有牌则依次输出牌
+            
+            printf("桌上的牌是: ");
+            for (i = 0; i <= s.top; i ++) {
+                printf(" %d", s.data[i]);
+            }
+        } else {
+            printf("桌子上没有牌");
+        }
+        
+    }
+    getchar();
+}
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
@@ -154,52 +372,19 @@ int main(int argc, const char * argv[]) {
          ws_queue();
          */
 
+        //Stack
+        //ws_stack();
+        
+        //小猫游戏
+        //ws_xiaomoyouxi();
         
         
-        char a[101], s[101];
-        
-        int i, len, mid, next, top;
-        
-        gets(a); //读入一行字符串
-//gets(<#char *#>)
-        scanf("%c", a);
-        
-        len = (int)strlen(a);
-        
-        mid = len/2 - 1;
+   
         
         
-        top = 0;//栈的初始化
-
-        //将mid前的字符串依次入栈
-        for (i = 0; i <= mid; i ++) {
-            s[++top] = a[i];
-        }
-        
-        //判断字符串数组的奇偶数, 并找出需要进行字符串匹配的起始下标
-        
-        if (len % 2 ==0) {
-            next = mid + 1;
-        } else {
-            next = mid + 2;
-        }
         
         
-        //开始匹配
         
-        for (i = next; i < len - 1; i ++) {
-            if (a[i] != s[top]) {
-                break;
-            }
-            top--;
-        }
-
-        //如果top为0 则说明每个数都检查过去了
-        if (top == 0) {
-            printf("YES");
-        } else {
-            printf("NO");
-        }
     }
     return 0;
 }
